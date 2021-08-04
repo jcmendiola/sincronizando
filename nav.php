a40f11db111d6ef8a82cd768a28d6e9ee397e9b3
Jorge Carlos Mendiola <mendiola57@gmail.com>
2021-08-04 08:28:45 -0300

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ol>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Quienes Somos</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Contacto</a></li>
+    </ol>
+</nav>
